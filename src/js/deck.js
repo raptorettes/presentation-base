c7fa3754e54b634331ff5098caf00f926d557385
Matthew McLeod <m@qwrk.dev>
2026-06-29 16:55:40 +0200

    notes

--- src/js/deck.js
+++ src/js/deck.js
@@ -1,6 +1,7 @@
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown'
 import Notes from 'reveal.js/plugin/notes'
+import Zoom from 'reveal.js/plugin/zoom'
 import Highlight from 'reveal.js/plugin/highlight'
 
 import 'reveal.js/reveal.css'
@@ -10,7 +11,7 @@ import 'reveal.js/plugin/highlight/monokai.css'
 let deck = new Reveal({
   hash: true,
   history: true,
-  plugins: [Markdown, Notes, Highlight],
+  plugins: [Markdown, Notes, Highlight, Zoom],
 })
 
 deck.initialize()
